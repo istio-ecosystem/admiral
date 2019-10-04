b8ee7d5443d8b53774f81369e527c5fe3be0cfb5
aattuluri <anil_attuluri@intuit.com>
2019-10-03 21:58:25 -0700

    Add some tests

--- admiral/pkg/controller/common/common_test.go
+++ admiral/pkg/controller/common/common_test.go
@@ -93,6 +93,12 @@ func TestGetSAN(t *testing.T) {
 			wantSAN: "spiffe://" + domain + "/" + identifierVal,
 		},
 		{
+			name: "should return valid SAN with no domain prefix",
+			deployment: deployment,
+			domain: "",
+			wantSAN: "spiffe://" + identifierVal,
+		},
+		{
 			name: "should return empty SAN",
 			deployment: deploymentWithNoIdentifier,
 			domain: domain,
@@ -143,6 +149,111 @@ func TestGetCname(t *testing.T) {
 			cname := GetCname(&c.deployment, identifier)
 			if !(cname == c.expected) {
 				t.Errorf("Wanted Cname: %s, got: %s", c.expected, cname)
+			}
+		})
+	}
+}
+
+func TestNodeLocality(t *testing.T) {
+
+	nodeLocalityLabel := "us-west-2"
+
+	testCases := []struct {
+		name   string
+		node   v12.Node
+		expected string
+	}{
+		{
+			name:    "should return valid node region",
+			node: v12.Node{Spec: v12.NodeSpec{}, ObjectMeta: v1.ObjectMeta{Labels:map[string]string{NodeRegionLabel: nodeLocalityLabel}}},
+			expected: nodeLocalityLabel,
+		},
+		{
+			name:    "should return empty value when node annotation isn't present",
+			node: v12.Node{Spec: v12.NodeSpec{}, ObjectMeta: v1.ObjectMeta{Annotations:map[string]string{}}},
+			expected: "",
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			locality := GetNodeLocality(&c.node)
+			if !(locality == c.expected) {
+				t.Errorf("Wanted locality: %s, got: %s", c.expected, locality)
+			}
+		})
+	}
+}
+
+func TestGetDeploymentGlobalIdentifier(t *testing.T) {
+
+	identifier := "identity"
+	identifierVal := "company.platform.server"
+
+	testCases := []struct {
+		name   string
+		deployment   k8sAppsV1.Deployment
+		expected string
+	}{
+		{
+			name:    "should return valid identifier from label",
+			deployment: k8sAppsV1.Deployment{Spec: k8sAppsV1.DeploymentSpec{Template:v12.PodTemplateSpec{ObjectMeta: v1.ObjectMeta{Labels:map[string]string{identifier: identifierVal, "env": "stage"}}}}},
+			expected: identifierVal,
+		},
+		{
+			name:    "should return valid identifier from annotations",
+			deployment: k8sAppsV1.Deployment{Spec: k8sAppsV1.DeploymentSpec{Template:v12.PodTemplateSpec{ObjectMeta: v1.ObjectMeta{Annotations:map[string]string{identifier: identifierVal, "env": "stage"}}}}},
+			expected: identifierVal,
+		},
+		{
+			name:    "should return empty identifier",
+			deployment: k8sAppsV1.Deployment{Spec: k8sAppsV1.DeploymentSpec{Template:v12.PodTemplateSpec{ObjectMeta: v1.ObjectMeta{Labels:map[string]string{}, Annotations:map[string]string{}}}}},
+			expected: "",
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			iVal := GetDeploymentGlobalIdentifier(&c.deployment)
+			if !(iVal == c.expected) {
+				t.Errorf("Wanted identity value: %s, got: %s", c.expected, iVal)
+			}
+		})
+	}
+}
+
+func TestGetPodGlobalIdentifier(t *testing.T) {
+
+	identifier := "identity"
+	identifierVal := "company.platform.server"
+
+	testCases := []struct {
+		name   string
+		pod   v12.Pod
+		expected string
+	}{
+		{
+			name:    "should return valid identifier from label",
+			pod: v12.Pod{Spec: v12.PodSpec{}, ObjectMeta: v1.ObjectMeta{Labels:map[string]string{identifier: identifierVal, "env": "stage"}}},
+			expected: identifierVal,
+		},
+		{
+			name:    "should return valid identifier from annotation",
+			pod: v12.Pod{Spec: v12.PodSpec{}, ObjectMeta: v1.ObjectMeta{Annotations:map[string]string{identifier: identifierVal, "env": "stage"}}},
+			expected: identifierVal,
+		},
+		{
+			name:    "should return empty identifier",
+			pod: v12.Pod{Spec: v12.PodSpec{}, ObjectMeta: v1.ObjectMeta{Annotations:map[string]string{}, Labels:map[string]string{}}},
+			expected: "",
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			iVal := GetPodGlobalIdentifier(&c.pod)
+			if !(iVal == c.expected) {
+				t.Errorf("Wanted identity value: %s, got: %s", c.expected, iVal)
 			}
 		})
 	}
