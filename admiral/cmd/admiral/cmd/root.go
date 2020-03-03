fa8716897a1c0a8227e23df35f6814844e7d4376
Joe Peacock <joe_peacock@intuit.com>
2020-03-02 16:45:53 -0800

    fixed bug in adding to cnameidentitycache and variably assignment bug in getting GTP identity
    Signed-off-by: Joe Peacock <joe_peacock@intuit.com>

--- admiral/cmd/admiral/cmd/root.go
+++ admiral/cmd/admiral/cmd/root.go
@@ -70,7 +70,7 @@ func GetRootCmd(args []string) *cobra.Command {
 	rootCmd.PersistentFlags().StringVar(&params.SecretResolver, "secret_resolver", "",
 		"Type of resolver to use to fetch kubeconfig for monitored clusters")
 	rootCmd.PersistentFlags().StringVar(&params.LabelSet.DeploymentAnnotation, "deployment_annotation", "sidecar.istio.io/inject",
-		"The annotation, on a deployment, which must be set to \"true\" for Admiral to listen on the deployment")
+		"The annotation, on a pod spec in a deployment, which must be set to \"true\" for Admiral to listen on the deployment")
 	rootCmd.PersistentFlags().StringVar(&params.LabelSet.SubsetLabel, "subset_label", "subset",
 		"The label, on a deployment, tells admiral which target group this deployment is a part of. Used for traffic splits via the Global Traffic Policy object")
 	rootCmd.PersistentFlags().StringVar(&params.LabelSet.NamespaceSidecarInjectionLabel, "namespace_injected_label", "istio-injection",
