4a2d161504a7deda6b56d7c4edafde5d7aa9fa7c
sa <sushanth_a@intuit.com>
2020-04-20 23:02:45 +0530

    Code refactor
    Signed-off-by: sa <sushanth_a@intuit.com>

--- admiral/pkg/clusters/util.go
+++ admiral/pkg/clusters/util.go
@@ -3,54 +3,31 @@ package clusters
 import (
 	"errors"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
-	argo "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 	"strconv"
 	"strings"
-
+	argo "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	k8sAppsV1 "k8s.io/api/apps/v1"
 	k8sV1 "k8s.io/api/core/v1"
 )
 
 func GetMeshPorts(clusterName string, destService *k8sV1.Service,
 	destDeployment *k8sAppsV1.Deployment) map[string]uint32 {
-	var ports = make(map[string]uint32)
 	var meshPorts = destDeployment.Spec.Template.Annotations[common.SidecarEnabledPorts]
-	if len(meshPorts) == 0 {
-		log.Infof(LogFormat, "GetMeshPorts", "service", destService.Name, clusterName, "No mesh ports present, defaulting to first port")
-		if destService.Spec.Ports != nil && len(destService.Spec.Ports) > 0 {
-			var name = destService.Spec.Ports[0].Name
-			if len(name) == 0 {
-				name = common.Http
-			}
-			ports[name] = uint32(destService.Spec.Ports[0].Port)
-		}
-		return ports
-	}
-
-	meshPortsSplit := strings.Split(meshPorts, ",")
-	var meshPortMap = make(map[uint32]uint32)
-	for _, meshPort := range meshPortsSplit {
-		port, err := strconv.ParseUint(meshPort, 10, 32)
-		if err != nil {
-			continue
-		}
-		meshPortMap[uint32(port)] = uint32(port)
-	}
-	for _, servicePort := range destService.Spec.Ports {
-		if _, ok := meshPortMap[uint32(servicePort.Port)]; ok {
-			log.Debugf(LogFormat, "GetMeshPorts", servicePort.Port, destService.Name, clusterName, "Adding mesh port")
-			ports[common.Http] = uint32(servicePort.Port)
-		}
-	}
+	ports := getMeshPortsHelper(meshPorts,destService,clusterName)
 	return ports
 }
 
 func GetMeshPortsForRollout(clusterName string, destService *k8sV1.Service,
 	destRollout *argo.Rollout) map[string]uint32 {
-	var ports = make(map[string]uint32)
 	var meshPorts = destRollout.Spec.Template.Annotations[common.SidecarEnabledPorts]
+	ports := getMeshPortsHelper(meshPorts,destService,clusterName)
+	return ports
+}
+
+func getMeshPortsHelper (meshPorts string,destService *k8sV1.Service,clusterName string ) map[string]uint32{
+	var ports = make(map[string]uint32)
 	if len(meshPorts) == 0 {
 		log.Infof(LogFormat, "GetMeshPorts", "service", destService.Name, clusterName, "No mesh ports present, defaulting to first port")
 		if destService.Spec.Ports != nil && len(destService.Spec.Ports) > 0 {
@@ -80,6 +57,8 @@ func GetMeshPortsForRollout(clusterName string, destService *k8sV1.Service,
 	}
 	return ports
 }
+
+
 
 func GetServiceEntryStateFromConfigmap(configmap *k8sV1.ConfigMap) *ServiceEntryAddressStore {
 
