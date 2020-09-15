dee9d1c7a2b872ee99588bfdde13986d43445c14
Mengying-Li <mengyinglimandy@gmail.com>
2020-09-15 14:51:25 -0700

    changed the vm driver to be none
    Signed-off-by: Mengying-Li <mengyinglimandy@gmail.com>

--- tests/create_cluster.sh
+++ tests/create_cluster.sh
@@ -5,5 +5,5 @@
 k8s_version=$1
 vm_driver=$2
 
-minikube start --memory=2048 --cpus=2 --kubernetes-version=$k8s_version --vm-driver $vm_driver
+minikube start --memory=2048 --cpus=2 --kubernetes-version=$k8s_version --vm-driver --vm-driver=none
 export KUBECONFIG=${HOME}/.kube/config
