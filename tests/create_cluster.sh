1ce64f9993205d145955cae2b92668c6bb1cd691
Mengying-Li <mengyinglimandy@gmail.com>
2020-09-15 15:03:03 -0700

    used the exact line to spin up k8s
    Signed-off-by: Mengying-Li <mengyinglimandy@gmail.com>

--- tests/create_cluster.sh
+++ tests/create_cluster.sh
@@ -5,5 +5,6 @@
 k8s_version=$1
 vm_driver=$2
 
-minikube start --memory=2048 --cpus=2 --kubernetes-version=$k8s_version --vm-driver --vm-driver=none
+#minikube start --memory=2048 --cpus=2 --kubernetes-version=$k8s_version --vm-driver --vm-driver=none
+sudo -E minikube start --vm-driver=none --cpus 2 --memory 2048 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
 export KUBECONFIG=${HOME}/.kube/config
