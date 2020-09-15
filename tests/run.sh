6bf1a36476186614349d9199469f8375e7a8cecd
Mengying-Li <mengyinglimandy@gmail.com>
2020-09-14 17:48:27 -0700

    updated version
    Signed-off-by: Mengying-Li <mengyinglimandy@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -9,7 +9,7 @@ install_dir=$3
 #source ./create_cluster.sh $k8s_version "virtualbox"
 ./install_istio.sh $istio_version "osx"
 # Uncomment below line if setup fails due to KUBECONFIG not set
-#export KUBECONFIG=~/.kube/config
+export KUBECONFIG=~/.kube/config
 ./dns_setup.sh $install_dir
 $install_dir/scripts/install_admiral.sh $install_dir
 $install_dir/scripts/install_rollouts.sh
@@ -18,7 +18,7 @@ $install_dir/scripts/install_sample_services.sh $install_dir
 
 #allow for stabilization of DNS and Istio SEs before running tests
 sleep 10
-./tests/test1.sh "webapp" "sample" "greeting"
-./tests/test2.sh "webapp" "sample-rollout-bluegreen" "greeting"
+./test1.sh "webapp" "sample" "greeting"
+./test2.sh "webapp" "sample-rollout-bluegreen" "greeting"
 
 ./cleanup.sh $istio_version
