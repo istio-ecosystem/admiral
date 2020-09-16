1e4d571004e53a3d3e20d6b4eea0fdf1dc733135
Mengying-Li <mengyinglimandy@gmail.com>
2020-09-16 11:44:42 -0700

    added the IS_LOCAL var
    Signed-off-by: Mengying-Li <mengyinglimandy@gmail.com>

--- tests/create_cluster.sh
+++ tests/create_cluster.sh
@@ -4,7 +4,14 @@
 
 k8s_version=$1
 vm_driver=$2
+printenv IS_LOCAL
+#minikube start --memory=8192 --cpus=4 --kubernetes-version=$k8s_version --vm-driver "virtualbox"
+#sudo -E minikube start --vm-driver=none --cpus 2 --memory 2048 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
+#
+if [[ $IS_LOCAL == "false" ]]; then
+  sudo -E minikube start --vm-driver=none --cpus 2 --memory 2048 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
+else
+  minikube start --memory=8192 --cpus=4 --kubernetes-version=$k8s_version --vm-driver "virtualbox"
+fi
 
-#minikube start --memory=2048 --cpus=2 --kubernetes-version=$k8s_version --vm-driver --vm-driver=none
-sudo -E minikube start --vm-driver=none --cpus 2 --memory 2048 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
 export KUBECONFIG=${HOME}/.kube/config
