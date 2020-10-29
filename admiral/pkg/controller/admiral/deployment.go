aea3cd9a5d339b6d3cfce7fddf7a70ad3caed06e
Mengying <mengyinglimandy@gmail.com>
2020-10-29 10:53:15 -0700

    added a little bit funnction for deleting deployment
    Signed-off-by: Mengying <mengyinglimandy@gmail.com>

--- admiral/pkg/controller/admiral/deployment.go
+++ admiral/pkg/controller/admiral/deployment.go
@@ -177,7 +177,15 @@ func HandleAddUpdateDeployment(ojb interface{}, d *DeploymentController) {
 }
 
 func (d *DeploymentController) Deleted(ojb interface{}) {
-	//TODO
+	HandleDeleteDeployment(ojb, d)
+}
+
+func HandleDeleteDeployment(ojb interface{}, d *DeploymentController) {
+	deployment := ojb.(*k8sAppsV1.Deployment)
+	key := d.Cache.getKey(deployment)
+	if len(key) > 0 {
+		d.Cache.DeleteFromDeploymentClusterCache(key, deployment)
+	}
 }
 
 func (d *DeploymentController) shouldIgnoreBasedOnLabels(deployment *k8sAppsV1.Deployment) bool {
