8b1198f9f38df77ac96c8cb8cddcf3012042dfba
aattuluri <anil_attuluri@intuit.com>
2021-01-12 15:22:18 -0800

    Run on larger circle ci instance, remove nodes

--- tests/create_cluster.sh
+++ tests/create_cluster.sh
@@ -5,7 +5,7 @@
 k8s_version=$1
 
 if [[ $IS_LOCAL == "false" ]]; then
-  sudo -E minikube start --nodes 2 --vm-driver=none --cpus 4 --memory 6144 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
+  sudo -E minikube start --vm-driver=none --cpus 4 --memory 8192 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
 else
   minikube start --memory=4096 --cpus=4 --kubernetes-version=$k8s_version --vm-driver "virtualbox"
 fi
