117c02f71ad404ecefc5a285b4a40423dd9d1dc0
aattuluri <anil_attuluri@intuit.com>
2022-01-23 01:27:21 -0800

    Change image to latest ubuntu

--- tests/create_cluster.sh
+++ tests/create_cluster.sh
@@ -6,15 +6,11 @@ k8s_version=$1
 
 echo "Creating K8s cluster with version: $k8s_version"
 
-minikube
-
-echo $IS_LOCAL
-
-echo $HOME
-
 if [[ $IS_LOCAL == "false" ]]; then
-  sudo -E minikube start --vm-driver=none --cpus 6 --memory 6144 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
+  sudo --help
+  sudo -E minikube start --vm-driver=docker --cpus 6 --memory 6144 --kubernetes-version=$k8s_version &> $HOME/minikube.log 2>&1 < /dev/null
 else
+  echo "Creating K8s cluster with virtualbox vm driver"
   minikube start --memory=4096 --cpus=4 --kubernetes-version=$k8s_version --vm-driver "virtualbox"
   #label node for locality load balancing
   kubectl label nodes minikube --overwrite failure-domain.beta.kubernetes.io/region=us-west-2
