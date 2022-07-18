f8f4441fcd550917ae713c85c7c6957561716887
sa <sushanth_a@intuit.com>
2022-07-18 22:19:04 +0530

    Attempt DCO fix

--- admiral/pkg/clusters/DRUtil.go
+++ admiral/pkg/clusters/DRUtil.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
 	log "github.com/sirupsen/logrus"
-	"strings"
 )
 const  ReadWriteEnabled = false
 const ReadOnlyEnabled = true;
@@ -41,7 +40,7 @@ utility function to identify the Admiral DR implementation based on the program 
 */
 func startAdmiralStateChecker (ctx context.Context,params common.AdmiralParams){
 	var  admiralStateChecker AdmiralStateChecker
-	admiralStateCheckerName :=  strings.ToLower(params.AdmiralStateCheckerName)
+	admiralStateCheckerName :=  params.AdmiralStateCheckerName
 	switch  admiralStateCheckerName {
 /*
      Add entries for your custom Disaster Recovery state checkers below
