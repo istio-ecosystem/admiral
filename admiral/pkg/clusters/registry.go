3df1ca2152053c5145b27f51316d3be9cba5dbab
Shriram Sharma <shriram_sharma@intuit.com>
2023-01-24 15:08:55 -0800

    made a code fix
    Signed-off-by: Shriram Sharma <shriram_sharma@intuit.com>

--- admiral/pkg/clusters/registry.go
+++ admiral/pkg/clusters/registry.go
@@ -42,15 +42,6 @@ func InitAdmiral(ctx context.Context, params common.AdmiralParams) (*RemoteRegis
 		return nil, fmt.Errorf("error with dependency controller init: %v", err)
 	}
 
-	dependencyProxyHandler := DependencyProxyHandler{
-		RemoteRegistry: w,
-	}
-
-	dependencyProxyHandler.DepController, err = admiral.NewDependencyProxyController(ctx.Done(), &dependencyProxyHandler, params.KubeconfigPath, params.DependenciesNamespace, params.CacheRefreshDuration)
-	if err != nil {
-		return nil, fmt.Errorf("error with dependencyproxy controller %w", err)
-	}
-
 	if !params.ArgoRolloutsEnabled {
 		log.Info("argo rollouts disabled")
 	}
