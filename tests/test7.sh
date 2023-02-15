ac4062133ef75aff75bbd4b1441b545a72be119c
Shriram Sharma <shriram_sharma@intuit.com>
2023-02-15 14:17:53 -0800

    fixed as per review comments
    Signed-off-by: Shriram Sharma <shriram_sharma@intuit.com>

--- tests/test7.sh
+++ tests/test7.sh
@@ -5,6 +5,13 @@
 ## The curl should validate httpbin external service is reachable
 ## through the proxy's virtualservice host
 
+## Request Flow:
+## GET http://stage.httpbin.foo/api/uuid =>
+##     Istio intercepts and routes to VS host stage.httpbin.foo =>
+##     VS host stage.httpbin.foo has a destination of SE host stage.httpbin.global:80 =>
+##     SE host stage.httpbin.global:80 forwards to proxy.svc.cluster.local:80 =>
+##     proxy.svc.cluster.local:80 which is an nginx gateway proxy, forwards the request to https://httpbin.org/uuid
+
 [ $# -lt 3 ] && { echo "Usage: $0 <source_app> <source_ns> <destination_vs_host>" ; exit 1; }
 
 source=$1
