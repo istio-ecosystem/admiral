00a1c5b7adbc6691fd3d4cbcd3e5657d0192ed41
Anubhav <anubhavaeron@gmail.com>
2024-06-12 19:23:11 -0700

    Update common.go

--- admiral/pkg/controller/common/common.go
+++ admiral/pkg/controller/common/common.go
@@ -2,16 +2,24 @@ package common
 
 import (
 	"bytes"
+	"context"
 	"crypto/sha1"
 	"encoding/gob"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/google/uuid"
 
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1"
+	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1alpha1"
 	log "github.com/sirupsen/logrus"
+
 	k8sAppsV1 "k8s.io/api/apps/v1"
 	k8sV1 "k8s.io/api/core/v1"
 )
@@ -23,49 +31,127 @@ var (
 )
 
 const (
-	NamespaceKubeSystem           = "kube-system"
-	NamespaceIstioSystem          = "istio-system"
-	Env                           = "env"
-	Http                          = "http"
-	Grpc                          = "grpc"
-	GrpcWeb                       = "grpc-web"
-	Http2                         = "http2"
-	DefaultMtlsPort               = 15443
-	DefaultServiceEntryPort       = 80
-	Sep                           = "."
-	Dash                          = "-"
-	Slash                         = "/"
-	DotLocalDomainSuffix          = ".svc.cluster.local"
-	Mesh                          = "mesh"
-	MulticlusterIngressGateway    = "istio-multicluster-ingressgateway"
-	LocalAddressPrefix            = "240.0"
-	NodeRegionLabel               = "failure-domain.beta.kubernetes.io/region"
-	SpiffePrefix                  = "spiffe://"
-	SidecarEnabledPorts           = "traffic.sidecar.istio.io/includeInboundPorts"
-	Default                       = "default"
-	AdmiralIgnoreAnnotation       = "admiral.io/ignore"
-	AdmiralCnameCaseSensitive     = "admiral.io/cname-case-sensitive"
-	BlueGreenRolloutPreviewPrefix = "preview"
-	RolloutPodHashLabel           = "rollouts-pod-template-hash"
-	RolloutActiveServiceSuffix    = "active-service"
-	RolloutStableServiceSuffix    = "stable-service"
-	WASMPath                      = "wasmPath"
+	NamespaceKubeSystem            = "kube-system"
+	NamespaceIstioSystem           = "istio-system"
+	IstioIngressGatewayServiceName = "istio-ingressgateway"
+	Env                            = "env"
+	Http                           = "http"
+	Grpc                           = "grpc"
+	GrpcWeb                        = "grpc-web"
+	Http2                          = "http2"
+	DefaultMtlsPort                = 15443
+	DefaultServiceEntryPort        = 80
+	Sep                            = "."
+	Dash                           = "-"
+	Slash                          = "/"
+	DotLocalDomainSuffix           = ".svc.cluster.local"
+	AssetAlias                     = "assetAlias"
+	Mesh                           = "mesh"
+	MulticlusterIngressGateway     = "istio-multicluster-ingressgateway"
+	LocalAddressPrefix             = "240.0"
+	NodeRegionLabel                = "failure-domain.beta.kubernetes.io/region"
+	SpiffePrefix                   = "spiffe://"
+	SidecarEnabledPorts            = "traffic.sidecar.istio.io/includeInboundPorts"
+	Default                        = "default"
+	SidecarInjectAnnotation        = "sidecar.istio.io/inject"
+	AdmiralIgnoreAnnotation        = "admiral.io/ignore"
+	AdmiralEnvAnnotation           = "admiral.io/env"
+	AdmiralCnameCaseSensitive      = "admiral.io/cname-case-sensitive"
+	BlueGreenRolloutPreviewPrefix  = "preview"
+	RolloutPodHashLabel            = "rollouts-pod-template-hash"
+	RolloutActiveServiceSuffix     = "active-service"
+	RolloutStableServiceSuffix     = "stable-service"
+	RolloutRootServiceSuffix       = "root-service"
+	CanaryRolloutCanaryPrefix      = "canary"
+	WASMPath                       = "wasmPath"
+	AdmiralProfileIntuit           = "intuit"
+	AdmiralProfileDefault          = "default"
+	AdmiralProfilePerf             = "perf"
+	Cartographer                   = "cartographer"
+	CreatedBy                      = "createdBy"
+	CreatedFor                     = "createdFor"
+	CreatedType                    = "createdType"
+	CreatedForEnv                  = "createdForEnv"
+	IsDisabled                     = "isDisabled"
+	TransactionID                  = "transactionID"
+	RevisionNumber                 = "revisionNumber"
+	EnvoyKind                      = "EnvoyFilter"
+	EnvoyApiVersion                = "networking.istio.io/v1alpha3"
+	SlashSTARRule                  = "/*"
+	AppThrottleConfigVersion       = "v1"
+	EnvoyFilterLogLevel            = "info"
+	EnvoyFilterLogLocation         = "proxy"
+	EnvoyFilterLogFormat           = "json"
+	ADD                            = "Add"
+	UPDATE                         = "Update"
+	DELETE                         = "Delete"
+	AssetLabel                     = "asset"
+
+	RollingWindow = "ROLLING_WINDOW"
+
+	MeshService              = "MESH_SERVICE"
+	Deployment               = "deployment"
+	Rollout                  = "rollout"
+	GTP                      = "gtp"
+	HAController             = "ha-controller"
+	EventType                = "eventType"
+	ProcessingInProgress     = "ProcessingInProgress"
+	NotProcessed             = "NotProcessed"
+	Processed                = "Processed"
+	ServicesGatewayIdentity  = "Intuit.platform.servicesgateway.servicesgateway"
+	DependentClusterOverride = "dependentClusterOverride"
+	Received                 = "Received"
+	Retry                    = "Retry"
+	Forget                   = "Forget"
+
+	ClusterName            = "clusterName"
+	EventResourceType      = "eventResourceType"
+	OutlierDetection       = "OutlierDetection"
+	ClientConnectionConfig = "ClientConnectionConfig"
+
+	WasmPathValue = "/etc/istio/extensions/dynamicrouter.wasm"
+	AIREnvSuffix  = "-air"
+	MESHSUFFIX    = ".mesh"
+
+	LastUpdatedAt = "lastUpdatedAt"
+	IntuitTID     = "intuit_tid"
+	GTPCtrl       = "gtp-ctrl"
 )
 
-type Event int
+type Event string
 
 const (
-	Add    Event = 0
-	Update Event = 1
-	Delete Event = 2
+	Add    Event = "Add"
+	Update Event = "Update"
+	Delete Event = "Delete"
 )
 
 type ResourceType string
 
 const (
-	VirtualService  ResourceType = "VirtualService"
-	DestinationRule ResourceType = "DestinationRule"
-	ServiceEntry    ResourceType = "ServiceEntry"
+	// Kubernetes/ARGO Resource Types
+	DeploymentResourceType ResourceType = "Deployment"
+	RolloutResourceType    ResourceType = "Rollout"
+	ServiceResourceType    ResourceType = "Service"
+	ConfigMapResourceType  ResourceType = "ConfigMap"
+	SecretResourceType     ResourceType = "Secret"
+	NodeResourceType       ResourceType = "Node"
+
+	// Admiral Resource Types
+	DependencyResourceType          ResourceType = "Dependency"
+	DependencyProxyResourceType     ResourceType = "DependencyProxy"
+	GlobalTrafficPolicyResourceType ResourceType = "GlobalTrafficPolicy"
+	RoutingPolicyResourceType       ResourceType = "RoutingPolicy"
+
+	// Istio Resource Types
+	VirtualServiceResourceType  ResourceType = "VirtualService"
+	DestinationRuleResourceType ResourceType = "DestinationRule"
+	ServiceEntryResourceType    ResourceType = "ServiceEntry"
+	EnvoyFilterResourceType     ResourceType = "EnvoyFilter"
+	SidecarResourceType         ResourceType = "Sidecar"
+
+	// Status
+	ReceivedStatus = "Received"
 )
 
 func GetPodGlobalIdentifier(pod *k8sV1.Pod) string {
@@ -82,7 +168,36 @@ func GetDeploymentGlobalIdentifier(deployment *k8sAppsV1.Deployment) string {
 		//TODO can this be removed now? This was for backward compatibility
 		identity = deployment.Spec.Template.Annotations[GetWorkloadIdentifier()]
 	}
+	if EnableSWAwareNSCaches() && len(identity) > 0 && len(GetDeploymentIdentityPartition(deployment)) > 0 {
+		identity = GetDeploymentIdentityPartition(deployment) + Sep + strings.ToLower(identity)
+
+	}
 	return identity
+}
+
+func GetDeploymentOriginalIdentifier(deployment *k8sAppsV1.Deployment) string {
+	identity := deployment.Spec.Template.Labels[GetWorkloadIdentifier()]
+	if len(identity) == 0 {
+		//TODO can this be removed now? This was for backward compatibility
+		identity = deployment.Spec.Template.Annotations[GetWorkloadIdentifier()]
+	}
+	return identity
+}
+
+func GetDeploymentIdentityPartition(deployment *k8sAppsV1.Deployment) string {
+	identityPartition := deployment.Spec.Template.Annotations[GetPartitionIdentifier()]
+	if len(identityPartition) == 0 {
+		//In case partition is accidentally applied as Label
+		identityPartition = deployment.Spec.Template.Labels[GetPartitionIdentifier()]
+	}
+	return identityPartition
+}
+
+func GetLocalDomainSuffix() string {
+	if IsAbsoluteFQDNEnabled() && IsAbsoluteFQDNEnabledForLocalEndpoints() {
+		return DotLocalDomainSuffix + Sep
+	}
+	return DotLocalDomainSuffix
 }
 
 // GetCname returns cname in the format <env>.<service identity>.global, Ex: stage.Admiral.services.registry.global
@@ -159,13 +274,7 @@ func GetValueForKeyFromDeployment(key string, deployment *k8sAppsV1.Deployment) 
 }
 
 func GetGtpEnv(gtp *v1.GlobalTrafficPolicy) string {
-	var environment = gtp.Annotations[GetEnvKey()]
-	if len(environment) == 0 {
-		environment = gtp.Labels[GetEnvKey()]
-	}
-	if len(environment) == 0 {
-		environment = gtp.Spec.Selector[GetEnvKey()]
-	}
+	environment := GetEnvFromMetadata(gtp.Annotations, gtp.Labels, gtp.Spec.Selector)
 	if len(environment) == 0 {
 		environment = gtp.Labels[Env]
 		log.Warnf("Using deprecated approach to use env label for GTP, name=%v in namespace=%v", gtp.Name, gtp.Namespace)
@@ -181,18 +290,14 @@ func GetGtpEnv(gtp *v1.GlobalTrafficPolicy) string {
 }
 
 func GetGtpIdentity(gtp *v1.GlobalTrafficPolicy) string {
-	identity := gtp.Labels[GetGlobalTrafficDeploymentLabel()]
-	if len(identity) == 0 {
-		identity = gtp.Spec.Selector[GetGlobalTrafficDeploymentLabel()]
-	}
-	return identity
+	return GetIdentity(gtp.Labels, gtp.Spec.Selector)
 }
 
 func GetGtpKey(gtp *v1.GlobalTrafficPolicy) string {
-	return ConstructGtpKey(GetGtpEnv(gtp), GetGtpIdentity(gtp))
+	return ConstructKeyWithEnvAndIdentity(GetGtpEnv(gtp), GetGtpIdentity(gtp))
 }
 
-func ConstructGtpKey(env, identity string) string {
+func ConstructKeyWithEnvAndIdentity(env, identity string) string {
 	return fmt.Sprintf("%s.%s", env, identity)
 }
 
@@ -237,13 +342,64 @@ func GetRoutingPolicyIdentity(rp *v1.RoutingPolicy) string {
 }
 
 func GetRoutingPolicyKey(rp *v1.RoutingPolicy) string {
-	return ConstructRoutingPolicyKey(GetRoutingPolicyEnv(rp), GetRoutingPolicyIdentity(rp))
+	return ConstructKeyWithEnvAndIdentity(GetRoutingPolicyEnv(rp), GetRoutingPolicyIdentity(rp))
 }
 
-// this function is exactly same as ConstructGtpKey.
+// this function is exactly same as ConstructKeyWithEnvAndIdentity.
 // Not reusing the same function to keep the methods associated with these two objects separate.
 func ConstructRoutingPolicyKey(env, identity string) string {
-	return fmt.Sprintf("%s.%s", env, identity)
+	return ConstructKeyWithEnvAndIdentity(env, identity)
+}
+
+func IsTrafficConfigDisabled(tc *v1.TrafficConfig) bool {
+	labelValue := tc.Labels["isDisabled"]
+	annotationValue := tc.Annotations["isDisabled"]
+	return labelValue == "true" || annotationValue == "true"
+}
+
+func GetTrafficConfigEnv(tc *v1.TrafficConfig) string {
+	identity := tc.Labels["env"]
+	if len(identity) == 0 {
+		identity = tc.Annotations["env"]
+	}
+	return identity
+}
+
+func GetTrafficConfigIdentity(tc *v1.TrafficConfig) string {
+	identity := tc.Labels[GetTrafficConfigIdentifier()]
+	if len(identity) == 0 {
+		identity = tc.Annotations[GetTrafficConfigIdentifier()]
+	}
+	return identity
+}
+
+func CheckIFEnvLabelIsPresent(tc *v1.TrafficConfig) error {
+	if tc.Labels == nil {
+		err := errors.New("no labels found of traffic config object - " + tc.Name + " in namespace - " + tc.Namespace)
+		return err
+	}
+	if len(tc.Labels["env"]) == 0 {
+		err := errors.New("mandatory label env is not present on the traffic config object=" + tc.Name + " in namespace=" + tc.Namespace)
+		return err
+	}
+	return nil
+}
+
+func GetTrafficConfigRevision(tc *v1.TrafficConfig) string {
+	identity := tc.Labels["revisionNumber"]
+	if len(identity) == 0 {
+		identity = tc.Annotations["revisionNumber"]
+	}
+	return identity
+}
+
+func GetTrafficConfigTransactionID(tc *v1.TrafficConfig) string {
+	tid := tc.Labels["transactionID"]
+	if len(tid) == 0 {
+		tid = tc.Annotations["transactionID"]
+	}
+	return tid
+
 }
 
 func GetSha1(key interface{}) (string, error) {
@@ -254,7 +410,11 @@ func GetSha1(key interface{}) (string, error) {
 	hasher := sha1.New()
 	hasher.Write(bv)
 	sha := hex.EncodeToString(hasher.Sum(nil))
-	return sha[0:5], nil
+	if len(sha) >= 20 {
+		return sha[0:20], nil
+	} else {
+		return sha, nil
+	}
 }
 
 func GetBytes(key interface{}) ([]byte, error) {
@@ -265,4 +425,187 @@ func GetBytes(key interface{}) ([]byte, error) {
 		return nil, err
 	}
 	return buf.Bytes(), nil
+}
+
+func AppendError(err error, newError error) error {
+	if newError != nil {
+		if err == nil {
+			err = newError
+		} else {
+			err = fmt.Errorf("%s; %s", err.Error(), newError.Error())
+		}
+	}
+	return err
+}
+
+func IsIstioIngressGatewayService(svc *k8sV1.Service) bool {
+	return svc.Namespace == NamespaceIstioSystem && svc.Name == IstioIngressGatewayServiceName
+}
+
+func FetchTxIdOrGenNew(ctx context.Context) string {
+	txId, ok := ctx.Value("txId").(string)
+	if !ok {
+		log.Errorf("unable to fetch txId from context, will recreate one")
+		id := uuid.New()
+		txId = id.String()
+	}
+	return txId
+}
+
+func GetCtxLogger(ctx context.Context, identity, env string) *log.Entry {
+	controllerName, ok := ctx.Value("controller").(string)
+	if ok {
+		triggeringCluster, ok := ctx.Value("cluster").(string)
+		if ok {
+			return log.WithFields(log.Fields{
+				"op":                "ConfigWriter",
+				"triggeringCluster": triggeringCluster,
+				"identity":          identity,
+				"txId":              FetchTxIdOrGenNew(ctx),
+				"controller":        controllerName,
+				"env":               env,
+			})
+		}
+		return log.WithFields(log.Fields{
+			"op":         "ConfigWriter",
+			"identity":   identity,
+			"txId":       FetchTxIdOrGenNew(ctx),
+			"controller": controllerName,
+			"env":        env,
+		})
+	}
+	return log.WithFields(log.Fields{
+		"op":       "ConfigWriter",
+		"identity": identity,
+		"txId":     FetchTxIdOrGenNew(ctx),
+		"env":      env,
+	})
+}
+
+func GetClientConnectionConfigIdentity(clientConnectionSettings *v1.ClientConnectionConfig) string {
+	return GetIdentity(clientConnectionSettings.Labels, map[string]string{})
+}
+
+func GetClientConnectionConfigEnv(clientConnectionSettings *v1.ClientConnectionConfig) string {
+	env := GetEnvFromMetadata(clientConnectionSettings.Annotations,
+		clientConnectionSettings.Labels, map[string]string{})
+	if len(env) == 0 {
+		env = clientConnectionSettings.Labels[Env]
+		log.Warnf("Using deprecated approach to use env label for %s, name=%v in namespace=%v",
+			ClientConnectionConfig, clientConnectionSettings.Name, clientConnectionSettings.Namespace)
+	}
+	if len(env) == 0 {
+		env = Default
+	}
+	return env
+}
+
+func GetIdentity(labels, selectors map[string]string) string {
+	identity := labels[GetAdmiralCRDIdentityLabel()]
+	if len(identity) == 0 {
+		identity = selectors[GetAdmiralCRDIdentityLabel()]
+	}
+	return identity
+}
+
+func GetEnvFromMetadata(annotations, labels, selectors map[string]string) string {
+	var env = annotations[GetEnvKey()]
+	if len(env) == 0 {
+		env = labels[GetEnvKey()]
+	}
+	if len(env) == 0 {
+		env = selectors[GetEnvKey()]
+	}
+	return env
+}
+
+func GetODIdentity(od *v1.OutlierDetection) string {
+	return GetIdentity(od.Labels, od.Spec.Selector)
+}
+
+func GetODEnv(od *v1.OutlierDetection) string {
+	env := GetEnvFromMetadata(od.Annotations, od.Labels, od.Spec.Selector)
+	if len(env) == 0 {
+		env = od.Labels[Env]
+		log.Warnf("Using deprecated approach to use env label for %s, name=%v in namespace=%v", OutlierDetection, od.Name, od.Namespace)
+	}
+	if len(env) == 0 {
+		env = od.Spec.Selector[Env]
+		log.Warnf("Using deprecated approach to use env label for %s, name=%v in namespace=%v", OutlierDetection, od.Name, od.Namespace)
+	}
+	if len(env) == 0 {
+		env = Default
+	}
+	return env
+}
+
+func RetryWithBackOff(ctx context.Context, callback func() error, retryCount int) error {
+	sleep := 10 * time.Second
+	var err error
+	for i := 0; i < retryCount; i++ {
+		if i > 0 {
+			log.Infof("retrying after sleeping %v, txId=%v", sleep, ctx.Value("txId"))
+			time.Sleep(sleep)
+			sleep *= 2
+		}
+		err = callback()
+		if err == nil {
+			break
+		}
+		log.Infof("retrying with error %v, txId=%v", err, ctx.Value("txId"))
+	}
+	return err
+}
+
+func SortGtpsByPriorityAndCreationTime(gtpsToOrder []*v1.GlobalTrafficPolicy, identity string, env string) {
+	sort.Slice(gtpsToOrder, func(i, j int) bool {
+		iPriority := getGtpPriority(gtpsToOrder[i])
+		jPriority := getGtpPriority(gtpsToOrder[j])
+
+		iTime := gtpsToOrder[i].CreationTimestamp
+		jTime := gtpsToOrder[j].CreationTimestamp
+		if iPriority != jPriority {
+			log.Debugf("GTP sorting identity=%s env=%s name1=%s creationTime1=%v priority1=%d name2=%s creationTime2=%v priority2=%d", identity, env, gtpsToOrder[i].Name, iTime, iPriority, gtpsToOrder[j].Name, jTime, jPriority)
+			return iPriority > jPriority
+		}
+		log.Debugf("GTP sorting identity=%s env=%s name1=%s creationTime1=%v priority1=%d name2=%s creationTime2=%v priority2=%d", identity, env, gtpsToOrder[i].Name, iTime, iPriority, gtpsToOrder[j].Name, jTime, jPriority)
+		return iTime.After(jTime.Time)
+	})
+}
+
+func getGtpPriority(gtp *v1.GlobalTrafficPolicy) int {
+	if val, ok := gtp.ObjectMeta.Labels[GetAdmiralParams().LabelSet.PriorityKey]; ok {
+		if convertedValue, err := strconv.Atoi(strings.TrimSpace(val)); err == nil {
+			return convertedValue
+		}
+	}
+	return 0
+}
+
+func GenerateTxId(meta v12.Object, ctrlName string, id string) string {
+	if meta != nil {
+		if ctrlName == GTPCtrl {
+			annotations := meta.GetAnnotations()
+			if len(annotations[IntuitTID]) > 0 {
+				id = annotations[IntuitTID] + "-" + id
+			}
+		}
+		if len(meta.GetResourceVersion()) > 0 {
+			id = meta.GetResourceVersion() + "-" + id
+		}
+	}
+	return id
+}
+
+func IsPresent(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func IsAirEnv(originalEnvLabel string) bool {
+	return strings.HasSuffix(originalEnvLabel, AIREnvSuffix)
 }
