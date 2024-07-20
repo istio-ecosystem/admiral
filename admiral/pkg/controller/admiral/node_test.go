d97cbd9f8cc581dedf57beb7803c30a291f5bffe
Shriram Sharma <shriram.sharma22@gmail.com>
2024-07-20 15:43:37 -0400

    copied admiral/pkg/controller/admiral/node_test.go from master
    Signed-off-by: Shriram Sharma <shriram_sharma@intuit.com>

--- admiral/pkg/controller/admiral/node_test.go
+++ admiral/pkg/controller/admiral/node_test.go
@@ -2,14 +2,70 @@ package admiral
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"github.com/istio-ecosystem/admiral/admiral/pkg/client/loader"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/test"
+	"github.com/stretchr/testify/assert"
 	k8sV1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+func TestNodeAddedTypeAssertion(t *testing.T) {
+
+	ctx := context.Background()
+	nodeController := NodeController{
+		Locality: &Locality{Region: "us-west-2"},
+	}
+
+	testCases := []struct {
+		name          string
+		node          interface{}
+		expectedError error
+	}{
+		{
+			name: "Given context and Node " +
+				"When Node param is nil " +
+				"Then func should return an error",
+			node:          nil,
+			expectedError: fmt.Errorf("type assertion failed, <nil> is not of type *v1.Node"),
+		},
+		{
+			name: "Given context and Node " +
+				"When Node param is not of type *v1.Node " +
+				"Then func should return an error",
+			node:          struct{}{},
+			expectedError: fmt.Errorf("type assertion failed, {} is not of type *v1.Node"),
+		},
+		{
+			name: "Given context and Node " +
+				"When Node param is of type *v1.Node " +
+				"Then func should not return an error",
+			node:          &k8sV1.Node{},
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			err := nodeController.Added(ctx, tc.node)
+			if tc.expectedError != nil {
+				assert.NotNil(t, err)
+				assert.Equal(t, tc.expectedError.Error(), err.Error())
+			} else {
+				if err != nil {
+					assert.Fail(t, "expected error to be nil but got %v", err)
+				}
+			}
+
+		})
+	}
+
+}
 
 func TestNewNodeController(t *testing.T) {
 	config, err := clientcmd.BuildConfigFromFlags("", "../../test/resources/admins@fake-cluster.k8s.local")
@@ -19,7 +75,7 @@ func TestNewNodeController(t *testing.T) {
 	stop := make(chan struct{})
 	handler := test.MockNodeHandler{}
 
-	nodeController, err := NewNodeController("", stop, &handler, config)
+	nodeController, err := NewNodeController(stop, &handler, config, loader.GetFakeClientLoader())
 
 	if err != nil {
 		t.Errorf("Unexpected err %v", err)
@@ -38,7 +94,7 @@ func TestNodeAddUpdateDelete(t *testing.T) {
 	stop := make(chan struct{})
 	handler := test.MockNodeHandler{}
 
-	nodeController, err := NewNodeController("", stop, &handler, config)
+	nodeController, err := NewNodeController(stop, &handler, config, loader.GetFakeClientLoader())
 
 	if err != nil {
 		t.Errorf("Unexpected err %v", err)
@@ -70,5 +126,51 @@ func TestNodeAddUpdateDelete(t *testing.T) {
 	//delete should make no difference
 	if locality.Region != region {
 		t.Errorf("region expected %v, got: %v", region, locality.Region)
+	}
+}
+
+// TODO: This is just a placeholder for when we add diff check for other types
+func TestNodeGetProcessItemStatus(t *testing.T) {
+	nodeController := NodeController{}
+	testCases := []struct {
+		name        string
+		obj         interface{}
+		expectedRes string
+	}{
+		{
+			name:        "TODO: Currently always returns false",
+			obj:         nil,
+			expectedRes: common.NotProcessed,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, _ := nodeController.GetProcessItemStatus(tc.obj)
+			assert.Equal(t, tc.expectedRes, res)
+		})
+	}
+}
+
+// TODO: This is just a placeholder for when we add diff check for other types
+func TestNodeUpdateProcessItemStatus(t *testing.T) {
+	nodeController := NodeController{}
+	testCases := []struct {
+		name        string
+		obj         interface{}
+		expectedErr error
+	}{
+		{
+			name:        "TODO: Currently always returns nil",
+			obj:         nil,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := nodeController.UpdateProcessItemStatus(tc.obj, common.NotProcessed)
+			assert.Equal(t, tc.expectedErr, err)
+		})
 	}
 }
