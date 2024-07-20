e2d248b724ca43cc07dddae766f4f5f754f2d4e1
Shriram Sharma <shriram.sharma22@gmail.com>
2024-07-20 15:35:47 -0400

    copied admiral/pkg/controller/admiral/dependencyproxy.go from master
    Signed-off-by: Shriram Sharma <shriram_sharma@intuit.com>

--- admiral/pkg/controller/admiral/dependencyproxy.go
+++ admiral/pkg/controller/admiral/dependencyproxy.go
@@ -6,19 +6,23 @@ import (
 	"sync"
 	"time"
 
-	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1"
+	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1alpha1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 
 	clientset "github.com/istio-ecosystem/admiral/admiral/pkg/client/clientset/versioned"
-	informerV1 "github.com/istio-ecosystem/admiral/admiral/pkg/client/informers/externalversions/admiral/v1"
+	informerV1 "github.com/istio-ecosystem/admiral/admiral/pkg/client/informers/externalversions/admiral/v1alpha1"
+	"github.com/istio-ecosystem/admiral/admiral/pkg/client/loader"
 )
 
 // DependencyProxyHandler interface contains the methods that are required
 type DependencyProxyHandler interface {
-	Added(ctx context.Context, obj *v1.DependencyProxy)
-	Updated(ctx context.Context, obj *v1.DependencyProxy)
-	Deleted(ctx context.Context, obj *v1.DependencyProxy)
+	Added(ctx context.Context, obj *v1.DependencyProxy) error
+	Updated(ctx context.Context, obj *v1.DependencyProxy) error
+	Deleted(ctx context.Context, obj *v1.DependencyProxy) error
 }
 
 type DependencyProxyController struct {
@@ -29,17 +33,26 @@ type DependencyProxyController struct {
 	informer               cache.SharedIndexInformer
 }
 
+type DependencyProxyItem struct {
+	DependencyProxy *v1.DependencyProxy
+	Status          string
+}
+
 type dependencyProxyCache struct {
 	//map of dependencies key=identity value array of onboarded identitys
-	cache map[string]*v1.DependencyProxy
+	cache map[string]*DependencyProxyItem
 	mutex *sync.Mutex
 }
 
 func (d *dependencyProxyCache) Put(dep *v1.DependencyProxy) {
 	defer d.mutex.Unlock()
 	d.mutex.Lock()
+
 	key := d.getKey(dep)
-	d.cache[key] = dep
+	d.cache[key] = &DependencyProxyItem{
+		DependencyProxy: dep,
+		Status:          common.ProcessingInProgress,
+	}
 }
 
 func (d *dependencyProxyCache) getKey(dep *v1.DependencyProxy) string {
@@ -49,7 +62,13 @@ func (d *dependencyProxyCache) getKey(dep *v1.DependencyProxy) string {
 func (d *dependencyProxyCache) Get(identity string) *v1.DependencyProxy {
 	defer d.mutex.Unlock()
 	d.mutex.Lock()
-	return d.cache[identity]
+
+	depItem, ok := d.cache[identity]
+	if ok {
+		return depItem.DependencyProxy
+	}
+
+	return nil
 }
 
 func (d *dependencyProxyCache) Delete(dep *v1.DependencyProxy) {
@@ -58,24 +77,55 @@ func (d *dependencyProxyCache) Delete(dep *v1.DependencyProxy) {
 	delete(d.cache, d.getKey(dep))
 }
 
-func NewDependencyProxyController(stopCh <-chan struct{}, handler DependencyProxyHandler, configPath string, namespace string, resyncPeriod time.Duration) (*DependencyProxyController, error) {
+func (d *dependencyProxyCache) GetDependencyProxyProcessStatus(dep *v1.DependencyProxy) string {
+	defer d.mutex.Unlock()
+	d.mutex.Lock()
+
+	key := d.getKey(dep)
+
+	depItem, ok := d.cache[key]
+	if ok {
+		return depItem.Status
+	}
+
+	return common.NotProcessed
+}
+
+func (d *dependencyProxyCache) UpdateDependencyProxyProcessStatus(dep *v1.DependencyProxy, status string) error {
+	defer d.mutex.Unlock()
+	d.mutex.Lock()
+
+	key := d.getKey(dep)
+
+	depItem, ok := d.cache[key]
+	if ok {
+		depItem.Status = status
+		d.cache[key] = depItem
+		return nil
+	}
+
+	return fmt.Errorf(LogCacheFormat, "Update", "DependencyProxy",
+		dep.Name, dep.Namespace, "", "nothing to update, dependency proxy not found in cache")
+}
+
+func NewDependencyProxyController(stopCh <-chan struct{}, handler DependencyProxyHandler, configPath string, namespace string, resyncPeriod time.Duration, clientLoader loader.ClientLoader) (*DependencyProxyController, error) {
 
 	controller := DependencyProxyController{}
 	controller.DependencyProxyHandler = handler
 
 	depProxyCache := dependencyProxyCache{}
-	depProxyCache.cache = make(map[string]*v1.DependencyProxy)
+	depProxyCache.cache = make(map[string]*DependencyProxyItem)
 	depProxyCache.mutex = &sync.Mutex{}
 
 	controller.Cache = &depProxyCache
 	var err error
 
-	controller.K8sClient, err = K8sClientFromPath(configPath)
+	controller.K8sClient, err = clientLoader.LoadKubeClientFromPath(configPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create dependency controller k8s client: %v", err)
 	}
 
-	controller.admiralCRDClient, err = AdmiralCrdClientFromPath(configPath)
+	controller.admiralCRDClient, err = clientLoader.LoadAdmiralClientFromPath(configPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create dependency controller crd client: %v", err)
 
@@ -88,26 +138,64 @@ func NewDependencyProxyController(stopCh <-chan struct{}, handler DependencyProx
 		cache.Indexers{},
 	)
 
-	mcd := NewMonitoredDelegator(&controller, "primary", "dependencyproxy")
-	NewController("dependencyproxy-ctrl-"+namespace, stopCh, mcd, controller.informer)
+	NewController("dependencyproxy-ctrl", "", stopCh, &controller, controller.informer)
 
 	return &controller, nil
 }
 
-func (d *DependencyProxyController) Added(ctx context.Context, ojb interface{}) {
-	dep := ojb.(*v1.DependencyProxy)
+func (d *DependencyProxyController) Added(ctx context.Context, obj interface{}) error {
+	dep, ok := obj.(*v1.DependencyProxy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.DependencyProxy", obj)
+	}
 	d.Cache.Put(dep)
-	d.DependencyProxyHandler.Added(ctx, dep)
+	return d.DependencyProxyHandler.Added(ctx, dep)
 }
 
-func (d *DependencyProxyController) Updated(ctx context.Context, obj interface{}, oldObj interface{}) {
-	dep := obj.(*v1.DependencyProxy)
+func (d *DependencyProxyController) Updated(ctx context.Context, obj interface{}, oldObj interface{}) error {
+	dep, ok := obj.(*v1.DependencyProxy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.DependencyProxy", obj)
+	}
 	d.Cache.Put(dep)
-	d.DependencyProxyHandler.Updated(ctx, dep)
+	return d.DependencyProxyHandler.Updated(ctx, dep)
 }
 
-func (d *DependencyProxyController) Deleted(ctx context.Context, ojb interface{}) {
-	dep := ojb.(*v1.DependencyProxy)
+func (d *DependencyProxyController) Deleted(ctx context.Context, obj interface{}) error {
+	dep, ok := obj.(*v1.DependencyProxy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.DependencyProxy", obj)
+	}
 	d.Cache.Delete(dep)
-	d.DependencyProxyHandler.Deleted(ctx, dep)
+	return d.DependencyProxyHandler.Deleted(ctx, dep)
+}
+
+func (d *DependencyProxyController) GetProcessItemStatus(obj interface{}) (string, error) {
+	dependencyProxy, ok := obj.(*v1.DependencyProxy)
+	if !ok {
+		return common.NotProcessed, fmt.Errorf("type assertion failed, %v is not of type *v1.DependencyProxy", obj)
+	}
+	return d.Cache.GetDependencyProxyProcessStatus(dependencyProxy), nil
+}
+
+func (d *DependencyProxyController) UpdateProcessItemStatus(obj interface{}, status string) error {
+	dependencyProxy, ok := obj.(*v1.DependencyProxy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.DependencyProxy", obj)
+	}
+	return d.Cache.UpdateDependencyProxyProcessStatus(dependencyProxy, status)
+}
+
+func (d *DependencyProxyController) LogValueOfAdmiralIoIgnore(obj interface{}) {
+}
+
+func (d *DependencyProxyController) Get(ctx context.Context, isRetry bool, obj interface{}) (interface{}, error) {
+	dependencyProxy, ok := obj.(*v1.DependencyProxy)
+	if ok && isRetry {
+		return d.Cache.Get(dependencyProxy.Name), nil
+	}
+	if ok && d.admiralCRDClient != nil {
+		return d.admiralCRDClient.AdmiralV1alpha1().DependencyProxies(dependencyProxy.Namespace).Get(ctx, dependencyProxy.Name, meta_v1.GetOptions{})
+	}
+	return nil, fmt.Errorf("admiralcrd client is not initialized, txId=%s", ctx.Value("txId"))
 }
