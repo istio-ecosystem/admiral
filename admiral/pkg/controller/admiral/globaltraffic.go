aabb1d376823f5c54e0208d3a9c2190c0800cb13
Shriram Sharma <shriram.sharma22@gmail.com>
2024-07-20 15:42:06 -0400

    copied admiral/pkg/controller/admiral/globaltraffic.go from master
    Signed-off-by: Shriram Sharma <shriram_sharma@intuit.com>

--- admiral/pkg/controller/admiral/globaltraffic.go
+++ admiral/pkg/controller/admiral/globaltraffic.go
@@ -6,7 +6,9 @@ import (
 	"sync"
 	"time"
 
-	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1"
+	"github.com/prometheus/common/log"
+
+	v1 "github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1alpha1"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
 	"github.com/sirupsen/logrus"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -14,14 +16,15 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	clientset "github.com/istio-ecosystem/admiral/admiral/pkg/client/clientset/versioned"
-	informerV1 "github.com/istio-ecosystem/admiral/admiral/pkg/client/informers/externalversions/admiral/v1"
+	informerV1 "github.com/istio-ecosystem/admiral/admiral/pkg/client/informers/externalversions/admiral/v1alpha1"
+	"github.com/istio-ecosystem/admiral/admiral/pkg/client/loader"
 )
 
 // GlobalTrafficHandler interface contains the methods that are required
 type GlobalTrafficHandler interface {
-	Added(ctx context.Context, obj *v1.GlobalTrafficPolicy)
-	Updated(ctx context.Context, obj *v1.GlobalTrafficPolicy)
-	Deleted(ctx context.Context, obj *v1.GlobalTrafficPolicy)
+	Added(ctx context.Context, obj *v1.GlobalTrafficPolicy) error
+	Updated(ctx context.Context, obj *v1.GlobalTrafficPolicy) error
+	Deleted(ctx context.Context, obj *v1.GlobalTrafficPolicy) error
 }
 
 type GlobalTrafficController struct {
@@ -31,9 +34,14 @@ type GlobalTrafficController struct {
 	informer             cache.SharedIndexInformer
 }
 
+type gtpItem struct {
+	GlobalTrafficPolicy *v1.GlobalTrafficPolicy
+	Status              string
+}
+
 type gtpCache struct {
 	//map of gtps key=identity+env value is a map of gtps namespace -> map name -> gtp
-	cache map[string]map[string]map[string]*v1.GlobalTrafficPolicy
+	cache map[string]map[string]map[string]*gtpItem
 	mutex *sync.Mutex
 }
 
@@ -43,16 +51,21 @@ func (p *gtpCache) Put(obj *v1.GlobalTrafficPolicy) {
 	key := common.GetGtpKey(obj)
 	namespacesWithGtps := p.cache[key]
 	if namespacesWithGtps == nil {
-		namespacesWithGtps = make(map[string]map[string]*v1.GlobalTrafficPolicy)
+		namespacesWithGtps = make(map[string]map[string]*gtpItem)
 	}
 	namespaceGtps := namespacesWithGtps[obj.Namespace]
 	if namespaceGtps == nil {
-		namespaceGtps = make(map[string]*v1.GlobalTrafficPolicy)
+		namespaceGtps = make(map[string]*gtpItem)
 	}
 	if common.ShouldIgnoreResource(obj.ObjectMeta) {
+		log.Infof("op=%s type=%v name=%v namespace=%s cluster=%s message=%s", "admiralIoIgnoreAnnotationCheck", common.GlobalTrafficPolicyResourceType,
+			obj.Name, obj.Namespace, "", "Value=true")
 		delete(namespaceGtps, obj.Name)
 	} else {
-		namespaceGtps[obj.Name] = obj
+		namespaceGtps[obj.Name] = &gtpItem{
+			GlobalTrafficPolicy: obj,
+			Status:              common.ProcessingInProgress,
+		}
 	}
 
 	namespacesWithGtps[obj.Namespace] = namespaceGtps
@@ -79,39 +92,82 @@ func (p *gtpCache) Delete(obj *v1.GlobalTrafficPolicy) {
 	p.cache[key] = namespacesWithGtps
 }
 
-//fetch gtps for a key from namespace
+// fetch gtps for a key from namespace
 func (p *gtpCache) Get(key, namespace string) []*v1.GlobalTrafficPolicy {
 	defer p.mutex.Unlock()
 	p.mutex.Lock()
 	namespacesWithGtp := p.cache[key]
 	matchedGtps := make([]*v1.GlobalTrafficPolicy, 0)
-	for ns, gtps := range namespacesWithGtp {
+	for ns, gtpItems := range namespacesWithGtp {
 		if namespace == ns {
-			for _, gtp := range gtps {
+			for _, item := range gtpItems {
 				logrus.Debugf("GTP match for identity=%s, from namespace=%v", key, ns)
 				//make a copy for safer iterations elsewhere
-				matchedGtps = append(matchedGtps, gtp.DeepCopy())
+				matchedGtps = append(matchedGtps, item.GlobalTrafficPolicy.DeepCopy())
 			}
 		}
 	}
 	return matchedGtps
 }
 
-func NewGlobalTrafficController(clusterID string, stopCh <-chan struct{}, handler GlobalTrafficHandler, configPath *rest.Config, resyncPeriod time.Duration) (*GlobalTrafficController, error) {
+func (p *gtpCache) GetGTPProcessStatus(gtp *v1.GlobalTrafficPolicy) string {
+	defer p.mutex.Unlock()
+	p.mutex.Lock()
+
+	key := common.GetGtpKey(gtp)
+
+	namespacesWithGtps, ok := p.cache[key]
+	if ok {
+		namespaceGtps, ok := namespacesWithGtps[gtp.Namespace]
+		if ok {
+			nameGtp, ok := namespaceGtps[gtp.Name]
+			if ok {
+				return nameGtp.Status
+			}
+		}
+	}
+
+	return common.NotProcessed
+}
+
+func (p *gtpCache) UpdateGTPProcessStatus(gtp *v1.GlobalTrafficPolicy, status string) error {
+	defer p.mutex.Unlock()
+	p.mutex.Lock()
+
+	key := common.GetGtpKey(gtp)
+
+	namespacesWithGtps, ok := p.cache[key]
+	if ok {
+		namespaceGtps, ok := namespacesWithGtps[gtp.Namespace]
+		if ok {
+			nameGtp, ok := namespaceGtps[gtp.Name]
+			if ok {
+				nameGtp.Status = status
+				p.cache[key] = namespacesWithGtps
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf(LogCacheFormat, "Update", "GTP",
+		gtp.Name, gtp.Namespace, "", "nothing to update, gtp not found in cache")
+}
+
+func NewGlobalTrafficController(stopCh <-chan struct{}, handler GlobalTrafficHandler, config *rest.Config, resyncPeriod time.Duration, clientLoader loader.ClientLoader) (*GlobalTrafficController, error) {
 
 	globalTrafficController := GlobalTrafficController{}
 
 	globalTrafficController.GlobalTrafficHandler = handler
 
 	gtpCache := gtpCache{}
-	gtpCache.cache = make(map[string]map[string]map[string]*v1.GlobalTrafficPolicy)
+	gtpCache.cache = make(map[string]map[string]map[string]*gtpItem)
 	gtpCache.mutex = &sync.Mutex{}
 
 	globalTrafficController.Cache = &gtpCache
 
 	var err error
 
-	globalTrafficController.CrdClient, err = AdmiralCrdClientFromConfig(configPath)
+	globalTrafficController.CrdClient, err = clientLoader.LoadAdmiralClientFromConfig(config)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create global traffic controller crd client: %v", err)
 	}
@@ -123,26 +179,78 @@ func NewGlobalTrafficController(clusterID string, stopCh <-chan struct{}, handle
 		cache.Indexers{},
 	)
 
-	mcd := NewMonitoredDelegator(&globalTrafficController, clusterID, "globaltrafficpolicy")
-	NewController("gtp-ctrl-"+configPath.Host, stopCh, mcd, globalTrafficController.informer)
+	NewController(common.GTPCtrl, config.Host, stopCh, &globalTrafficController, globalTrafficController.informer)
 
 	return &globalTrafficController, nil
 }
 
-func (d *GlobalTrafficController) Added(ctx context.Context, ojb interface{}) {
-	gtp := ojb.(*v1.GlobalTrafficPolicy)
+func (d *GlobalTrafficController) Added(ctx context.Context, obj interface{}) error {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.GlobalTrafficPolicy", obj)
+	}
 	d.Cache.Put(gtp)
-	d.GlobalTrafficHandler.Added(ctx, gtp)
+	return d.GlobalTrafficHandler.Added(ctx, gtp)
 }
 
-func (d *GlobalTrafficController) Updated(ctx context.Context, ojb interface{}, oldObj interface{}) {
-	gtp := ojb.(*v1.GlobalTrafficPolicy)
+func (d *GlobalTrafficController) Updated(ctx context.Context, obj interface{}, oldObj interface{}) error {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.GlobalTrafficPolicy", obj)
+	}
 	d.Cache.Put(gtp)
-	d.GlobalTrafficHandler.Updated(ctx, gtp)
+	return d.GlobalTrafficHandler.Updated(ctx, gtp)
 }
 
-func (d *GlobalTrafficController) Deleted(ctx context.Context, ojb interface{}) {
-	gtp := ojb.(*v1.GlobalTrafficPolicy)
+func (d *GlobalTrafficController) Deleted(ctx context.Context, obj interface{}) error {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.GlobalTrafficPolicy", obj)
+	}
 	d.Cache.Delete(gtp)
-	d.GlobalTrafficHandler.Deleted(ctx, gtp)
+	return d.GlobalTrafficHandler.Deleted(ctx, gtp)
+}
+
+func (d *GlobalTrafficController) GetProcessItemStatus(obj interface{}) (string, error) {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return common.NotProcessed, fmt.Errorf("type assertion failed, %v is not of type *v1.GlobalTrafficPolicy", obj)
+	}
+	return d.Cache.GetGTPProcessStatus(gtp), nil
+}
+
+func (d *GlobalTrafficController) UpdateProcessItemStatus(obj interface{}, status string) error {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return fmt.Errorf("type assertion failed, %v is not of type *v1.GlobalTrafficPolicy", obj)
+	}
+	return d.Cache.UpdateGTPProcessStatus(gtp, status)
+}
+
+func (d *GlobalTrafficController) LogValueOfAdmiralIoIgnore(obj interface{}) {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if !ok {
+		return
+	}
+	metadata := gtp.ObjectMeta
+	if metadata.Annotations[common.AdmiralIgnoreAnnotation] == "true" || metadata.Labels[common.AdmiralIgnoreAnnotation] == "true" {
+		log.Infof("op=%s type=%v name=%v namespace=%s cluster=%s message=%s", "admiralIoIgnoreAnnotationCheck", common.GlobalTrafficPolicyResourceType,
+			gtp.Name, gtp.Namespace, "", "Value=true")
+	}
+}
+
+func (d *GlobalTrafficController) Get(ctx context.Context, isRetry bool, obj interface{}) (interface{}, error) {
+	gtp, ok := obj.(*v1.GlobalTrafficPolicy)
+	if ok && isRetry {
+		orderedGtps := d.Cache.Get(common.GetGtpKey(gtp), gtp.Namespace)
+		if len(orderedGtps) == 0 {
+			return nil, fmt.Errorf("no gtps found for identity=%s, namespace=%s", common.GetGtpIdentity(gtp), gtp.Namespace)
+		}
+		common.SortGtpsByPriorityAndCreationTime(orderedGtps, common.GetGtpIdentity(gtp), common.GetGtpEnv(gtp))
+		return orderedGtps[0], nil
+	}
+	if ok && d.CrdClient != nil {
+		return d.CrdClient.AdmiralV1alpha1().GlobalTrafficPolicies(gtp.Namespace).Get(ctx, gtp.Name, meta_v1.GetOptions{})
+	}
+	return nil, fmt.Errorf("kubernetes client is not initialized, txId=%s", ctx.Value("txId"))
 }
