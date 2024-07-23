93306d43e35669cad8a3810c61bd0a88cc141679
kpharasi <kartik.230@gmail.com>
2024-07-23 15:52:16 -0700

    copy config.go from main branch

--- admiral/pkg/controller/common/config.go
+++ admiral/pkg/controller/common/config.go
@@ -1,33 +1,47 @@
 package common
 
 import (
-	"github.com/istio-ecosystem/admiral/admiral/pkg/monitoring"
+	"strings"
+	"sync"
 	"time"
 
+	"github.com/istio-ecosystem/admiral/admiral/pkg/monitoring"
+	"github.com/istio-ecosystem/admiral/admiral/pkg/util"
 	"github.com/matryer/resync"
 	log "github.com/sirupsen/logrus"
 )
 
-var admiralParams = AdmiralParams{
-	LabelSet: &LabelSet{},
+type admiralParamsWrapper struct {
+	params AdmiralParams
+	sync.RWMutex
+	resync.Once
 }
 
-var once resync.Once
+// Singleton
+var wrapper = admiralParamsWrapper{
+	params: AdmiralParams{
+		LabelSet: &LabelSet{},
+	},
+}
 
 func ResetSync() {
-	once.Reset()
+	wrapper.Reset()
 }
 
 func InitializeConfig(params AdmiralParams) {
 	var initHappened = false
-	once.Do(func() {
-		admiralParams = params
-		initHappened = true
-		InitializeMetrics()
+	wrapper.Do(func() {
+		wrapper.Lock()
+		defer wrapper.Unlock()
+		wrapper.params = params
+		if wrapper.params.LabelSet == nil {
+			wrapper.params.LabelSet = &LabelSet{}
+		}
 		err := monitoring.InitializeMonitoring()
 		if err != nil {
 			log.Errorf("failed to setup monitoring: %v", err)
 		}
+		initHappened = true
 	})
 	if initHappened {
 		log.Info("InitializeConfig was called.")
@@ -37,108 +51,410 @@ func InitializeConfig(params AdmiralParams) {
 }
 
 func GetAdmiralParams() AdmiralParams {
-	return admiralParams
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params
+}
+
+func GetAdmiralProfile() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.Profile
 }
 
 func GetArgoRolloutsEnabled() bool {
-	return admiralParams.ArgoRolloutsEnabled
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.ArgoRolloutsEnabled
+}
+
+func GetSecretFilterTags() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.SecretFilterTags
 }
 
 func GetKubeconfigPath() string {
-	return admiralParams.KubeconfigPath
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.KubeconfigPath
 }
 
 func GetCacheRefreshDuration() time.Duration {
-	return admiralParams.CacheRefreshDuration
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.CacheReconcileDuration
 }
 
 func GetClusterRegistriesNamespace() string {
-	return admiralParams.ClusterRegistriesNamespace
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.ClusterRegistriesNamespace
 }
 
 func GetDependenciesNamespace() string {
-	return admiralParams.DependenciesNamespace
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DependenciesNamespace
 }
 
 func GetSyncNamespace() string {
-	return admiralParams.SyncNamespace
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.SyncNamespace
 }
 
 func GetEnableSAN() bool {
-	return admiralParams.EnableSAN
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableSAN
 }
 
 func GetSANPrefix() string {
-	return admiralParams.SANPrefix
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.SANPrefix
 }
 
-func GetSecretResolver() string {
-	return admiralParams.SecretResolver
+func GetAdmiralConfigPath() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.AdmiralConfig
 }
 
 func GetLabelSet() *LabelSet {
-	return admiralParams.LabelSet
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet
 }
 
 func GetAdditionalEndpointSuffixes() []string {
-	return admiralParams.AdditionalEndpointSuffixes
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.AdditionalEndpointSuffixes
 }
 
 func GetAdditionalEndpointLabelFilters() []string {
-	return admiralParams.AdditionalEndpointLabelFilters
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.AdditionalEndpointLabelFilters
+}
+
+func GetEnableWorkloadDataStorage() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableWorkloadDataStorage
 }
 
 func GetHostnameSuffix() string {
-	return admiralParams.HostnameSuffix
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.HostnameSuffix
 }
 
 func GetWorkloadIdentifier() string {
-	return admiralParams.LabelSet.WorkloadIdentityKey
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.WorkloadIdentityKey
 }
 
-func GetGlobalTrafficDeploymentLabel() string {
-	return admiralParams.LabelSet.GlobalTrafficDeploymentLabel
+func GetPartitionIdentifier() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.IdentityPartitionKey
+}
+
+func GetTrafficConfigIdentifier() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.TrafficConfigIdentityKey
+}
+
+func GetAdmiralCRDIdentityLabel() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.AdmiralCRDIdentityLabel
 }
 
 func GetRoutingPolicyLabel() string {
-	return admiralParams.LabelSet.WorkloadIdentityKey
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.WorkloadIdentityKey
 }
 
 func GetWorkloadSidecarUpdate() string {
-	return admiralParams.WorkloadSidecarUpdate
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.WorkloadSidecarUpdate
 }
 
-func GetEnvoyFilterVersion() string {
-	return admiralParams.EnvoyFilterVersion
+func GetEnvoyFilterVersion() []string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	if len(strings.TrimSpace(wrapper.params.EnvoyFilterVersion)) == 0 {
+		return []string{}
+	}
+	return strings.Split(wrapper.params.EnvoyFilterVersion, ",")
+}
+
+func GetDeprecatedEnvoyFilterVersion() []string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	if len(strings.TrimSpace(wrapper.params.DeprecatedEnvoyFilterVersion)) == 0 {
+		return []string{}
+	}
+	return strings.Split(wrapper.params.DeprecatedEnvoyFilterVersion, ",")
 }
 
 func GetEnvoyFilterAdditionalConfig() string {
-	return admiralParams.EnvoyFilterAdditionalConfig
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnvoyFilterAdditionalConfig
 }
 
 func GetEnableRoutingPolicy() bool {
-	return admiralParams.EnableRoutingPolicy
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableRoutingPolicy
 }
 
 func GetWorkloadSidecarName() string {
-	return admiralParams.WorkloadSidecarName
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.WorkloadSidecarName
 }
 
 func GetEnvKey() string {
-	return admiralParams.LabelSet.EnvKey
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.LabelSet.EnvKey
 }
 
 func GetMetricsEnabled() bool {
-	return admiralParams.MetricsEnabled
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.MetricsEnabled
 }
 
-///Setters - be careful
+func IsPersonaTrafficConfig() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.TrafficConfigPersona
+}
+
+// This function is used to determine if a feature is enabled or not.
+// If the feature is not present in the list, it is assumed to be enabled.
+// Also any value other than "disabled" is assumed to be enabled.
+func IsCartographerFeatureDisabled(featureName string) bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+
+	if wrapper.params.CartographerFeatures == nil {
+		return false
+	}
+	// If the feature exists in the list and is set to disabled, return true
+	if val, ok := wrapper.params.CartographerFeatures[featureName]; ok {
+		return val == "disabled"
+	} else {
+		return false
+	}
+}
+
+func IsDefaultPersona() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return !wrapper.params.TrafficConfigPersona
+}
+
+func GetHAMode() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.HAMode
+}
+
+func GetDiffCheckEnabled() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableDiffCheck
+}
+
+func IsProxyEnvoyFilterEnabled() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableProxyEnvoyFilter
+}
+
+func IsDependencyProcessingEnabled() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableDependencyProcessing
+}
+
+func GetSeAddressConfigMap() string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.SeAddressConfigmap
+}
+
+func DeploymentOrRolloutWorkerConcurrency() int {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DeploymentOrRolloutWorkerConcurrency
+}
+
+func DependentClusterWorkerConcurrency() int {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DependentClusterWorkerConcurrency
+}
+
+func DependencyWarmupMultiplier() int {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DependencyWarmupMultiplier
+}
+
+func MaxRequestsPerConnection() int32 {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.MaxRequestsPerConnection
+}
+
+func IsAbsoluteFQDNEnabled() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableAbsoluteFQDN
+}
+
+func IsClientConnectionConfigProcessingEnabled() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableClientConnectionConfigProcessing
+}
+
+func IsAbsoluteFQDNEnabledForLocalEndpoints() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableAbsoluteFQDNForLocalEndpoints
+}
+
+func DisableDefaultAutomaticFailover() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DisableDefaultAutomaticFailover
+}
+
+func EnableServiceEntryCache() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableServiceEntryCache
+}
+
+func EnableDestinationRuleCache() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableDestinationRuleCache
+}
+
+func AlphaIdentityList() []string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.AlphaIdentityList
+}
 
 func SetKubeconfigPath(path string) {
-	admiralParams.KubeconfigPath = path
+	wrapper.Lock()
+	defer wrapper.Unlock()
+	wrapper.params.KubeconfigPath = path
 }
 
-// for unit test only
 func SetEnablePrometheus(value bool) {
-	admiralParams.MetricsEnabled = value
+	wrapper.Lock()
+	defer wrapper.Unlock()
+	wrapper.params.MetricsEnabled = value
+}
+
+func SetArgoRolloutsEnabled(value bool) {
+	wrapper.Lock()
+	defer wrapper.Unlock()
+	wrapper.params.ArgoRolloutsEnabled = value
+}
+
+func SetCartographerFeature(featureName string, val string) {
+	wrapper.Lock()
+	defer wrapper.Unlock()
+	if wrapper.params.CartographerFeatures == nil {
+		wrapper.params.CartographerFeatures = make(map[string]string)
+	}
+	wrapper.params.CartographerFeatures[featureName] = val
+}
+
+func GetGatewayAssetAliases() []string {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.GatewayAssetAliases
+}
+
+func DisableIPGeneration() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DisableIPGeneration
+}
+
+func EnableActivePassive() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableActivePassive
+}
+
+func EnableExportTo(identityOrCname string) bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	if wrapper.params.ExportToIdentityList != nil {
+		for _, identity := range wrapper.params.ExportToIdentityList {
+			if identity != "" && (identity == "*" || strings.Contains(strings.ToLower(identityOrCname), strings.ToLower(identity))) && wrapper.params.EnableSWAwareNSCaches {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func EnableSWAwareNSCaches() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableSWAwareNSCaches
+}
+
+func DoSyncIstioResourcesToSourceClusters() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.EnableSyncIstioResourcesToSourceClusters
+}
+
+func GetResyncIntervals() util.ResyncIntervals {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return util.ResyncIntervals{
+		UniversalReconcileInterval: wrapper.params.CacheReconcileDuration,
+		SeAndDrReconcileInterval:   wrapper.params.SeAndDrCacheReconcileDuration,
+	}
+}
+
+func GetExportToMaxNamespaces() int {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.ExportToMaxNamespaces
+}
+
+func IsAdmiralStateSyncerMode() bool {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.AdmiralStateSyncerMode
+}
+
+func GetDefaultWarmupDurationSecs() int64 {
+	wrapper.RLock()
+	defer wrapper.RUnlock()
+	return wrapper.params.DefaultWarmupDurationSecs
 }
