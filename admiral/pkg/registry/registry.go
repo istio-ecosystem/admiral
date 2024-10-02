311250fac5baac5c0dbce56f319021bcf72ba003
Ryan Tay <ryan_tay@intuit.com>
2024-10-02 14:07:25 -0700

    first pass at registry integration
    Signed-off-by: Ryan Tay <ryan_tay@intuit.com>

--- admiral/pkg/registry/registry.go
+++ admiral/pkg/registry/registry.go
@@ -2,6 +2,7 @@ package registry
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -42,38 +43,85 @@ func WithBaseClientConfig(clientConfig *Config) func(*registryClient) {
 	}
 }
 
-func (c *registryClient) PutClusterGateway(cluster, name, url, notes, tid string, labels []string) error {
-	r, err := http.NewRequest("PUT", url, nil)
-	authHeader := r.Header.Get("Authorization")
-	authData, err := ParsePrivateAuthHeader(authHeader)
-	response, err := c.client.MakePrivateAuthCall(url, authData, tid, r.Method, nil)
+func marshalDataForRegistry(data map[string]interface{}, url string) ([]byte, error) {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal json body for http request to %s", url)
+	}
+	if body == nil {
+		return nil, fmt.Errorf("json body for request to %s was nil", url)
+	}
+	return body, nil
+}
+
+func makeCallToRegistry(url, tid, method string, data map[string]interface{}, client BaseClient) error {
+	var body []byte
+	var err error
+	var response *http.Response
+	if data != nil {
+		body, err = marshalDataForRegistry(data, url)
+		if err != nil {
+			return err
+		}
+	}
+	response, err = client.MakePrivateAuthCall(url, tid, method, body)
+	if err != nil {
+		return err
+	}
+	if response == nil {
+		return fmt.Errorf("response for http request to %s was nil", url)
+	}
 	return nil
-	//sort labels?
+}
+
+func (c *registryClient) PutClusterGateway(cluster, name, ingressURL, notes, resourceType, tid string, labels []string) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/gateway/%s?type=%s", cluster, name, resourceType)
+	data := map[string]interface{}{
+		"url":   ingressURL,
+		"label": labels, // sort labels?
+		"notes": notes,  // not sure what the point of this is
+	}
+	return makeCallToRegistry(url, tid, http.MethodPut, data, c.client)
 	//preferably put this in service handler where we check if common.IsIstioIngressGatewayService(svc)
 }
 
-func (c *registryClient) DeleteClusterGateway(cluster, name string) error {
-	return nil
+func (c *registryClient) DeleteClusterGateway(cluster, name, resourceType, tid string) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/gateway/%s?type=%s", cluster, name, resourceType)
+	return makeCallToRegistry(url, tid, http.MethodDelete, nil, c.client)
 }
 
-func (c *registryClient) PutCustomData(cluster, namespace, key string, value interface{}) error {
-	return nil
-	// switch-case based on key (CCC/GTP/VS/OD/etc)
+func (c *registryClient) PutCustomData(cluster, name, namespace, resourceType, tid string, value interface{}) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/namespaces/%s/customdata?type=%s", cluster, namespace, resourceType)
+	data := map[string]interface{}{
+		"name":  name,         // name of the object in the cluster, not assetAlias
+		"type":  resourceType, // GlobalTrafficPolicy/CCC/OD/VS/etc
+		"value": value,        // could be the entire yaml or whatever we want here
+	}
+	return makeCallToRegistry(url, tid, http.MethodPut, data, c.client)
+	// switch-case based on type (CCC/GTP/VS/OD/etc) to cast value?
 	// traffic config?
 }
 
-func (c *registryClient) DeleteCustomData(cluster, namespace, key string) error {
-	return nil
+func (c *registryClient) DeleteCustomData(cluster, name, namespace, resourceType, tid string) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/namespaces/%s/customdata/%s?type=%s", cluster, namespace, name, resourceType)
+	return makeCallToRegistry(url, tid, http.MethodDelete, nil, c.client)
 }
 
-func (c *registryClient) PutHostingData(cluster, namespace, key string, value interface{}) error {
-	return nil
-	// switch-case based on key (service/rollout/deployment)
+func (c *registryClient) PutHostingData(cluster, name, namespace, assetAlias, resourceType, tid string, metadata map[string]interface{}) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/namespaces/%s/hosting/%s", cluster, namespace, name)
+	data := map[string]interface{}{
+		common.AssetAlias: assetAlias,
+		"name":            name,         // name of the object in the cluster ie env-assetAlias-rollout
+		"type":            resourceType, // service/deployment/rollout/etc
+		"metadata":        metadata,     // this will be filled in differently for service/deployment/rollout/etc
+	}
+	return makeCallToRegistry(url, tid, http.MethodPut, data, c.client)
 	// Where does sidecar and envoy filter go?
 }
 
-func (c *registryClient) DeleteHostingData(cluster, namespace, key string) error {
-	return nil
+func (c *registryClient) DeleteHostingData(cluster, name, namespace, resourceType, tid string) error {
+	url := fmt.Sprintf("/v1/k8s/clusters/%s/namespaces/%s/hosting/%s?type=%s", cluster, namespace, name, resourceType)
+	return makeCallToRegistry(url, tid, http.MethodDelete, nil, c.client)
 }
 
 // GetIdentityConfigByIdentityName calls the registry API to fetch the IdentityConfig for
