a52f5678dc4d82f09acb5791655df19bc005d022
Adil Fulara <adil_fulara@intuit.com>
2024-12-23 17:02:17 -0800

    Tech-Debt: Increase coverage
    - 100% coverage for clientdiscovery_handler
    
    Signed-off-by: Adil Fulara <adil_fulara@intuit.com>

--- admiral/pkg/clusters/clientdiscovery_handler_test.go
+++ admiral/pkg/clusters/clientdiscovery_handler_test.go
@@ -2,19 +2,21 @@ package clusters
 
 import (
 	"context"
+	"errors"
+	argo "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/model"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/apis/admiral/v1alpha1"
+	commonUtil "github.com/istio-ecosystem/admiral/admiral/pkg/util"
 	"github.com/stretchr/testify/assert"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 	"time"
 
-	argo "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/client/loader"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/admiral"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/controller/common"
 	"github.com/istio-ecosystem/admiral/admiral/pkg/test"
-	coreV1 "k8s.io/api/core/v1"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -22,157 +24,304 @@ func TestClientDiscoveryHandler_Added(t *testing.T) {
 	//Struct of test case info. Name is required.
 	const (
 		namespace           = "namespace"
-		namespace_no_deps   = "namespace-no-deps"
+		namespaceNoDeps     = "namespace-no-deps"
 		namespaceForRollout = "rollout-namespace"
 		cluster1            = "cluster1"
 		cluster2            = "cluster2"
-		cluster3            = "cluster3"
 		identity1           = "identity1"
 		identity2           = "identity2"
-		identity3           = "identity3"
 		identity4           = "identity4"
 	)
 	var (
-		stop   = make(chan struct{})
-		config = rest.Config{
-			Host: "localhost",
-		}
-		matchLabel = map[string]string{
-			"app": "test",
-		}
-		labelSelector = metaV1.LabelSelector{
-			MatchLabels: matchLabel,
-		}
-		dep = v1alpha1.Dependency{
-			Spec: model.Dependency{
-				Source:       identity1,
-				Destinations: []string{identity2},
-			},
-		}
-		rollout = argo.Rollout{
-			Spec: argo.RolloutSpec{
-				Selector: &labelSelector,
-				Strategy: argo.RolloutStrategy{
-					BlueGreen: &argo.BlueGreenStrategy{},
-				},
-				Template: coreV1.PodTemplateSpec{
-					ObjectMeta: metaV1.ObjectMeta{Annotations: map[string]string{
-						"sidecar.istio.io/inject": "true",
-						"identity":                identity1,
-					}, Labels: map[string]string{
-						"env": "stage",
-					}},
-				},
-			},
-			ObjectMeta: metaV1.ObjectMeta{
-				Namespace: namespaceForRollout,
-			},
-		}
-		objRolloutNamespace = &common.K8sObject{
-			Namespace:   namespaceForRollout,
-			Name:        "obj-rollout-namespace",
-			Annotations: map[string]string{"identity": identity1},
-		}
-		objValid = &common.K8sObject{
-			Namespace:   namespace,
-			Name:        "obj",
-			Annotations: map[string]string{"identity": identity1},
-		}
-		objValidWithNoDeps = &common.K8sObject{
-			Namespace:   namespace_no_deps,
-			Name:        "obj-no-deps",
-			Annotations: map[string]string{"identity": identity4},
-		}
+		stop = make(chan struct{})
 	)
 
-	setupForHandlerTests(true)
-
-	depController, err := admiral.NewDependencyController(stop, &test.MockDependencyHandler{}, loader.FakeKubeconfigPath, "", time.Second*time.Duration(300), loader.GetFakeClientLoader())
-	if err != nil {
-		t.Fatalf("failed to initialize dependency controller, err: %v", err)
-	}
-	r, err := admiral.NewRolloutsController(stop, &test.MockRolloutHandler{}, &config, time.Second*time.Duration(300), loader.GetFakeClientLoader())
-	if err != nil {
-		t.Fatalf("failed to initialize rollout controller, err: %v", err)
-	}
-	d, err := admiral.NewDeploymentController(stop, &test.MockDeploymentHandler{}, &config, time.Second*time.Duration(300), loader.GetFakeClientLoader())
-	if err != nil {
-		t.Fatalf("failed to initialize rollout controller, err: %v", err)
+	type args struct {
+		obj              *common.K8sObject
+		options          []Option
+		rollout          *argo.Rollout
+		dep              *v1alpha1.Dependency
+		readOnly         bool
+		disableCache     bool
+		admiralReadyOnly bool
+		depCtrlAddErr    error
 	}
 
-	rcCluster1 := &RemoteController{
-		RolloutController:    r,
-		DeploymentController: d,
+	type want struct {
+		identityClusterNSCacheLen int
+		partitionIdentityCacheLen int
+		err                       error
+		depCtrlAddCalled          int
 	}
-
-	rcCluster2 := &RemoteController{
-		RolloutController:    r,
-		DeploymentController: d,
-	}
-
-	ctx := context.Background()
-
-	remoteRegistry := NewRemoteRegistry(context.TODO(), common.AdmiralParams{})
-	remoteRegistry.DependencyController = depController
-	remoteRegistry.remoteControllers[cluster1] = rcCluster1
-	remoteRegistry.remoteControllers[cluster2] = rcCluster2
-
-	remoteRegistry.AdmiralCache = &AdmiralCache{
-		IdentityClusterCache:          common.NewMapOfMaps(),
-		IdentityClusterNamespaceCache: common.NewMapOfMapOfMaps(),
-		PartitionIdentityCache:        common.NewMap(),
-	}
-
-	err = r.Added(ctx, &rollout)
-	assert.NoError(t, err)
-	rolloutFromCache := r.Cache.Get(identity1, "stage")
-	assert.NotNil(t, rolloutFromCache)
-	err = depController.Added(ctx, &dep)
-	assert.NoError(t, err)
-	depFromCache := depController.Cache.Get(identity1)
-	assert.NotNil(t, depFromCache)
 
 	testCases := []struct {
-		name          string
-		obj           *common.K8sObject
-		rr            *RemoteRegistry
-		clusterName   string
-		expectedError error
+		name string
+		args args
+		want want
 	}{
 		{
-			name:          "No global identifier on obj results in no errors",
-			obj:           &common.K8sObject{Annotations: map[string]string{}, Labels: map[string]string{}},
-			rr:            remoteRegistry,
-			clusterName:   cluster1,
-			expectedError: nil,
-		}, {
+			name: "No global identifier on obj results in no errors",
+			args: args{
+				obj: &common.K8sObject{Annotations: map[string]string{}, Labels: map[string]string{}},
+			},
+			want: want{},
+		},
+		{
+			name: "Admiral Cache is not initialized returns error",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespaceForRollout,
+					Name:        "obj-rollout-namespace",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				disableCache: true,
+			},
+			want: want{
+				err: errors.New("op=Add type=Job name=obj-rollout-namespace cluster=cluster1 error=task=Add name=obj-rollout-namespace namespace=rollout-namespace cluster=cluster1 message=processing skipped as Admiral cache is not initialized for identity=identity1"),
+			},
+		},
+		{
+			name: "Admiral in Read Only mode updates SW aware caches",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespaceForRollout,
+					Name:        "obj-rollout-namespace",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				options:  []Option{argoEnabled(true), swAwareNsCache(true)},
+				readOnly: true,
+			},
+			want: want{
+				identityClusterNSCacheLen: 1,
+				partitionIdentityCacheLen: 1,
+			},
+		},
+		{
+			name: "Admiral during cache warmup returns early",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespaceForRollout,
+					Name:        "obj-rollout-namespace",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				options: []Option{
+					argoEnabled(true),
+					swAwareNsCache(true),
+					cacheWarmupTime(1 * time.Minute),
+				},
+			},
+			want: want{
+				identityClusterNSCacheLen: 1,
+				partitionIdentityCacheLen: 1,
+			},
+		},
+		{
 			name: "Rollout with same identifier present in the namespace should " +
 				"return without processing and no errors",
-			obj:           objRolloutNamespace,
-			rr:            remoteRegistry,
-			clusterName:   cluster1,
-			expectedError: nil,
-		}, {
-			name:          "Valid client with no dependency record returns without processing",
-			obj:           objValidWithNoDeps,
-			rr:            remoteRegistry,
-			clusterName:   cluster1,
-			expectedError: nil,
-		}, {
-			name:          "Valid client with valid dependency record results in writing all dependencies endpoints to this cluster",
-			obj:           objValid,
-			rr:            remoteRegistry,
-			clusterName:   cluster1,
-			expectedError: nil,
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespaceForRollout,
+					Name:        "obj-rollout-namespace",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				rollout: &argo.Rollout{
+					Spec: argo.RolloutSpec{
+						Selector: &metaV1.LabelSelector{
+							MatchLabels: map[string]string{
+								"app": "test",
+							},
+						},
+						Strategy: argo.RolloutStrategy{
+							BlueGreen: &argo.BlueGreenStrategy{},
+						},
+						Template: coreV1.PodTemplateSpec{
+							ObjectMeta: metaV1.ObjectMeta{Annotations: map[string]string{
+								"sidecar.istio.io/inject": "true",
+								"identity":                identity1,
+							}, Labels: map[string]string{
+								"env": "stage",
+							}},
+						},
+					},
+					ObjectMeta: metaV1.ObjectMeta{
+						Namespace: namespaceForRollout,
+					},
+				},
+				options: []Option{argoEnabled(true), swAwareNsCache(true), dependencyProcessing(true)},
+			},
+			want: want{
+				identityClusterNSCacheLen: 1,
+				partitionIdentityCacheLen: 1,
+			},
+		},
+		{
+			name: "Valid client with no dependency record returns without processing",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespaceNoDeps,
+					Name:        "obj-no-deps",
+					Annotations: map[string]string{"identity": identity4},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				options: []Option{
+					argoEnabled(true),
+					swAwareNsCache(true)},
+			},
+			want: want{
+				identityClusterNSCacheLen: 1,
+				partitionIdentityCacheLen: 1,
+			},
+		},
+		{
+			name: "Valid client with valid dependency record results in calling depHandler Added()",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespace,
+					Name:        "obj",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				options: []Option{
+					argoEnabled(true),
+				},
+				dep: &v1alpha1.Dependency{
+					Spec: model.Dependency{
+						Source:       identity1,
+						Destinations: []string{identity2},
+					},
+				},
+			},
+			want: want{
+				depCtrlAddCalled: 1,
+			},
+		},
+		{
+			name: "Valid client with valid dependency record with error in calling depHandler Added() returns error",
+			args: args{
+				obj: &common.K8sObject{
+					Namespace:   namespace,
+					Name:        "obj",
+					Annotations: map[string]string{"identity": identity1},
+					Labels: map[string]string{
+						"admiral.io/identityPartition": "test",
+					},
+				},
+				options: []Option{
+					argoEnabled(true),
+				},
+				dep: &v1alpha1.Dependency{
+					Spec: model.Dependency{
+						Source:       identity1,
+						Destinations: []string{identity2},
+					},
+				},
+				depCtrlAddErr: errors.New("oops"),
+			},
+			want: want{
+				depCtrlAddCalled: 1,
+				err:              errors.New("op=Add type=Job name=obj cluster=cluster1 error=op=Process type= name=obj namespace=namespace cluster=cluster1 message=Error processing client discovery"),
+			},
 		},
 	}
 
 	//Run the test for every provided case
 	for _, c := range testCases {
 		t.Run(c.name, func(t *testing.T) {
-			result := HandleEventForClientDiscovery(ctx, admiral.Add, c.obj, remoteRegistry, c.clusterName)
-			if c.expectedError == nil && result != nil {
-				t.Fatalf("Expected error %v got %v", c.expectedError, result)
+
+			// Given
+			ctx := context.Background()
+			InitAdmiralForTesting(c.args.options...)
+
+			mockDepCtrl := test.NewMockDependencyHandler(c.args.depCtrlAddErr, nil, nil)
+			depController, err := admiral.NewDependencyController(stop, mockDepCtrl, loader.FakeKubeconfigPath, "", time.Second*time.Duration(300), loader.GetFakeClientLoader())
+			if err != nil {
+				t.Fatalf("failed to initialize dependency controller, err: %v", err)
+			}
+			config := rest.Config{
+				Host: "localhost",
+			}
+			r, err := admiral.NewRolloutsController(stop, &test.MockRolloutHandler{}, &config, time.Second*time.Duration(300), loader.GetFakeClientLoader())
+			if err != nil {
+				t.Fatalf("failed to initialize rollout controller, err: %v", err)
+			}
+			if c.args.rollout != nil {
+				entry := &admiral.RolloutClusterEntry{
+					Identity: "test." + identity1,
+					Rollouts: map[string]*admiral.RolloutItem{identity1: {Rollout: c.args.rollout}},
+				}
+				r.Cache.Put(entry)
+				err = r.Added(ctx, c.args.rollout)
+				assert.NoError(t, err)
+			}
+			if c.args.dep != nil {
+				depController.Cache.Put(c.args.dep)
+			}
+			d, err := admiral.NewDeploymentController(stop, &test.MockDeploymentHandler{}, &config, time.Second*time.Duration(300), loader.GetFakeClientLoader())
+			if err != nil {
+				t.Fatalf("failed to initialize rollout controller, err: %v", err)
+			}
+
+			rcCluster1 := &RemoteController{
+				RolloutController:    r,
+				DeploymentController: d,
+			}
+
+			rcCluster2 := &RemoteController{
+				RolloutController:    r,
+				DeploymentController: d,
+			}
+
+			rr := NewRemoteRegistry(context.TODO(), common.AdmiralParams{})
+			rr.DependencyController = depController
+			rr.remoteControllers[cluster1] = rcCluster1
+			rr.remoteControllers[cluster2] = rcCluster2
+
+			rr.AdmiralCache = &AdmiralCache{
+				IdentityClusterCache:          common.NewMapOfMaps(),
+				IdentityClusterNamespaceCache: common.NewMapOfMapOfMaps(),
+				PartitionIdentityCache:        common.NewMap(),
+			}
+
+			commonUtil.CurrentAdmiralState.ReadOnly = c.args.readOnly
+			if c.args.disableCache {
+				rr.AdmiralCache = nil
+			}
+
+			h := &ClientDiscoveryHandler{ClusterID: cluster1, RemoteRegistry: rr}
+			// When
+			result := h.Added(ctx, c.args.obj)
+
+			// Then
+			if c.want.err != nil && assert.Error(t, result) {
+				// error validation
+				assert.Equal(t, result, c.want.err)
+			} else {
+				assert.Equal(t, c.want.partitionIdentityCacheLen, rr.AdmiralCache.PartitionIdentityCache.Len())
+				assert.Equal(t, c.want.identityClusterNSCacheLen, rr.AdmiralCache.IdentityClusterNamespaceCache.Len())
+				if c.args.rollout != nil {
+					rolloutFromCache := r.Cache.Get(identity1, "stage")
+					assert.NotNil(t, rolloutFromCache)
+				}
+			}
+
+			if c.want.depCtrlAddCalled > 0 {
+				assert.Equal(t, c.want.depCtrlAddCalled, mockDepCtrl.AddedCnt)
 			}
 		})
 	}
